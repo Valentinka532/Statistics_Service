f520826393784feaa99f894a8c099f33d85de8ca
Valentina <valentina100392@yandex.ru>
2024-11-11 00:16:58 +0300

    JaCoCo

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,7 @@
 package ru.netology.statistic;
 
-public class StatisticsService {    public long findMax(long[] incomes) {
+public class StatisticsService {
+    public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes) {
             if (currentMax < income) {
